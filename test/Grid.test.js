76c37420232f4811521ba8377048b149ab1253c7
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-06-22 00:46:00 +0100

    added remove obstacle test

--- test/Grid.test.js
+++ test/Grid.test.js
@@ -35,3 +35,29 @@ test('add obstacles.', () => {
     expect(obstacle4).toBe("1")
 
 });
+
+test('remove obstacle.', () => {
+    let grid = new Grid([10, 10], [0, 0], [9, 9]);
+    
+    let start = grid.getNode([0,0]).toString();
+    let finish = grid.getNode([9,9]).toString();
+    expect(start).toBe("2");
+    expect(finish).toBe("3");
+    
+    grid.addObstacle([9,7]);
+    grid.addObstacle([8,7]);
+    grid.addObstacle([6,7]);
+    grid.addObstacle([6,8]);
+
+    grid.removeObstacle([6,8]);
+
+    let obstacle1 = grid.getNode([9,7]).toString();
+    let obstacle2 = grid.getNode([8,7]).toString();
+    let obstacle3 = grid.getNode([6,7]).toString();
+    let obstacle4 = grid.getNode([6,8]).toString();
+
+    expect(obstacle1).toBe("1")
+    expect(obstacle2).toBe("1")
+    expect(obstacle3).toBe("1")
+    expect(obstacle4).toBe("0")
+})
