1b7323e273560269778c4918e424be9882fa3ea4
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-06-22 11:50:57 +0100

    + started pathfinding

---
+++ src/Pathfinder.ts
@@ -0,0 +1,55 @@
+import { resourceLimits } from "worker_threads";
+import Grid, { Coordinate } from "./Grid";
+
+
+function distance(coordA: Coordinate, coordB: Coordinate) {
+    // Formula: sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    let [x1, y1] = coordA;
+    let [x2, y2] = coordB;
+
+    let x = Math.abs(x2 - x1);
+    x = x * x;
+
+    let y = Math.abs(y2 - y1);
+    y = y * y;
+
+    let z = x + y;
+    z = Math.sqrt(z);
+
+    return z;
+}
+
+function toArray(json:any){
+    let result = [];
+
+    for(let key in json){
+        result.push(json[key])
+    }
+
+    return result;
+}
+
+class Pathfinder {
+    constructor(grid: Grid){
+        let start = grid.start;
+        let finish = grid.finish;
+        let startNode = grid.getNode(start);
+        let finishNode = grid.getNode(finish);
+        let neigbourhood = startNode.neigbours;
+        let neigbours = toArray(neigbourhood);
+
+        neigbours.forEach(neigbour => {
+            if(neigbour === undefined){ return; }
+            neigbour.setGCost((distance(start, neigbour.position)) * 10);
+            neigbour.setHCost((distance(finish, neigbour.position)) * 10);
+        });
+    }
+}
+
+
+
+export default Pathfinder;
+
+export {
+    distance
+}
